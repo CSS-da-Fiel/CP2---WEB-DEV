ccea61b562086cdc61bcdb67882fa57de7d98336
matheus <matheusfreitas2k25@gmail.com>
2025-11-01 01:28:26 -0300

    4- Estruturando quiz.html

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+//FUNÇÃO TROCAR
+function trocar(cor){
+  document.body.style.background=cor;  
+}
+
+//FUNÇÃO SLIDESHOW
+
+// DECLARANDO UM ARRAY DE IMAGENS
+
+let imagens=[
+    "src/assets/garro1.jpg",
+];
+
+//DECLARANDO AS VARIAVEIS
+
+let i=0;
+let tempo =5000; // milisegundos
+
+//função 
+
+function slideShow(){
+    document.getElementById("image").src=imagens[i];
+    i++;
+    if(i == imagens.length){
+        i=0;
+    }
+    setTimeout(slideShow,tempo)
+}
+slideShow();
